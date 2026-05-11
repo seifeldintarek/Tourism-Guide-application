4582d1a9096d1801bcde7d828fafa1b49bfc1d81
seifeldintarek <seiftarek211200@gmail.com>
2026-05-11 21:16:28 +0300

    remove dark theme

--- lib/root/app_root.dart
+++ lib/root/app_root.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/root/themes.dart';
 import 'package:flutter_application_1/screens/login/login.dart';
+import 'package:provider/provider.dart';
 
 class AppRoot extends StatelessWidget {
   const AppRoot({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final localeProvider = context.watch<LocaleProvider>();
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true),
-      home: Login(),
+      locale: localeProvider.locale,
+      supportedLocales: const [
+        Locale('en'),
+        Locale('ar'),
+        Locale('fr'),
+        Locale('es'),
+        Locale('de'),
+      ],
+      home: const Login(),
     );
   }
 }
--- lib/root/themes.dart
+++ lib/root/themes.dart
@@ -1,37 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class ThemeProvider extends ChangeNotifier {
-  bool isDarkMode = false;
-
-  ThemeProvider() {
-    _loadTheme();
-  }
-
-  void toggleTheme() {
-    isDarkMode = !isDarkMode;
-    _saveTheme(isDarkMode);
-    notifyListeners();
-  }
-
-  Future<void> _loadTheme() async {
-    final prefs = await SharedPreferences.getInstance();
-    isDarkMode = prefs.getBool('isDarkMode') ?? false;
-    notifyListeners();
-  }
-
-  Future<void> _saveTheme(bool value) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool('isDarkMode', value);
-  }
-
-  void setTheme(bool value) {
-    isDarkMode = value;
-    _saveTheme(value);
-    notifyListeners();
-  }
-}
-
 class LocaleProvider with ChangeNotifier {
   Locale _locale = const Locale('en');
 
