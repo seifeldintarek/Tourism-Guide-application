2884a81a59787f291e0715eaa32c746909217c0c
seifeldintarek <seiftarek211200@gmail.com>
2026-05-11 20:13:03 +0300

    firebase setup completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/firebase_options.dart';
 import 'package:flutter_application_1/root/app_root.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  print("firebase initialized successfully");
+
   runApp(const MainApp());
 }
 
